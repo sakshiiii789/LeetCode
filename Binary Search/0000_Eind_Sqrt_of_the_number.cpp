448bdc886e6620dcc10f8c4c7c00bef7ff034965
Sakshi Yadav <sakshi.01yt@gmail.com>
2026-08-10 00:53:10 +0530

    Add 0000 Find Sqrt of the number

---
+++ Binary Search/0000_Eind_Sqrt_of_the_number.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    int mySqrt(int x){
+        if(x<2) return x;
+        int low=1;int high=x/2; int ans=0;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if(mid*mid<=x){
+                ans=mid;
+                low=mid+1;
+
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
